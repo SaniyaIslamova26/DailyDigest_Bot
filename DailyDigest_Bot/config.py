465f8d7981f775e622b3cea63d1b8132a18b3dc2
SaniyaIslamova26 <saniya.islamova.02@mail.ru>
2025-12-14 23:38:50 +0300

    Add files via upload

---
+++ DailyDigest_Bot/config.py
@@ -0,0 +1,3 @@
+# Конфигурационный файл проекта
+BOT_TOKEN = "8526104408:AAESuXT62QcB5P8l1wduqDgro8DlZwvP2Lw"  # Токен Telegram-бота
+ADMIN_ID = 695384856                                          # ID администратора системы
